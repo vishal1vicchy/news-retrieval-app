9af052e4f23a605a0a1471fbcd4180fbaf4c4dd8
vempatisaivishal <vempatisaivishal@gmail.com>
2024-08-28 18:56:11 +0530

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,72 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component, createRef } from 'react';
+import Navbar from './components/Navbar';
+import News from './components/News';
+import LoadingBar from 'react-top-loading-bar';
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from 'react-router-dom';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default class App extends Component {
+  constructor(props) {
+    super(props);
+    this.ref = createRef();
+  }
+  pageSize = 5;
+  apiKey = process.env.REACT_APP_NEWS_API
+
+  state = {
+    progress:0
+  }
+  setProgress = (progress) => {
+    this.ref.current.continuousStart();
+    setTimeout(() => {
+      this.ref.current.complete();
+    }, progress);
+  };
+
+  render() {
+    const router = createBrowserRouter([
+      {
+        path: '/',
+        element: <News pageSize={5} country="in" category="general" setProgress={this.setProgress}/>,
+      },
+      {
+        path: '/business',
+        element: <News pageSize={5} country="in" category="business" setProgress={this.setProgress}/>,
+      },
+      {
+        path: '/entertainment',
+        element: <News pageSize={5} country="in" category="entertainment" setProgress={this.setProgress}/>,
+      },
+      {
+        path: '/general',
+        element: <News pageSize={5} country="in" category="general" setProgress={this.setProgress}/>,
+      },
+      {
+        path: '/health',
+        element: <News pageSize={5} country="in" category="health" setProgress={this.setProgress}/>,
+      },
+      {
+        path: '/science',
+        element: <News pageSize={5} country="in" category="science" setProgress={this.setProgress}/>,
+      },
+      {
+        path: '/sports',
+        element: <News pageSize={5} country="in" category="sports" setProgress={this.setProgress}/>,
+      },
+      {
+        path: '/technology',
+        element: <News pageSize={5} country="in" category="technology" setProgress={this.setProgress}/>,
+      },
+    ]);
+
+    return (
+      <div>
+        <Navbar />
+        <LoadingBar color='#f11946' ref={this.ref} />
+        <RouterProvider router={router} />
+      </div>
+    );
+  }
 }
-
-export default App;
